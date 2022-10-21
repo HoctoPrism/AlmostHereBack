d7fe7593d785ab621864a9a376f4e141976f0a2f
HoctoPrism <nemexxix@gmail.com>
2022-10-21 18:49:54 +0200

    ajout des champs pour le controller register

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -24,14 +24,25 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
+            'lastname' => 'required|string|max:191',
+            'firstname' => 'required|string|max:191',
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
+        if (empty($request->role)) {
+            $request->role = json_encode(["ROLE_USER"]);
+        } else {
+            json_encode($request->role);
+        }
+
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'lastname' => $request->lastname,
+            'firstname' => $request->firstname,
             'password' => Hash::make($request->password),
+            'role' => $request->role
         ]);
 
         event(new Registered($user));
