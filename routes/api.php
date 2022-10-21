a878c22cba36102d205d9aae052ead280040abb6
HoctoPrism <nemexxix@gmail.com>
2022-10-21 15:50:42 +0200

    definition des routes pour l'api // WIP middleware auth

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\API\AgencyController;
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\CalendarController;
+use App\Http\Controllers\API\CalendarDatesController;
+use App\Http\Controllers\API\FavoritesController;
+use App\Http\Controllers\API\FrequenciesController;
+use App\Http\Controllers\API\MessagesController;
+use App\Http\Controllers\API\RoutesController;
+use App\Http\Controllers\API\ShapesController;
+use App\Http\Controllers\API\StopsController;
+use App\Http\Controllers\API\StopTimesController;
+use App\Http\Controllers\API\TripsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +25,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+    Route::get('current-user', 'currentUser');
 });
+
+Route::controller(FavoritesController::class)->group(function () {
+    Route::get('favorites', 'index');
+    Route::get('favorites/{favorite}', 'show');
+    Route::post('favorites', 'store');
+    Route::patch('favorites/{favorite}', 'update');
+    Route::delete('favorites/{favorite}', 'destroy');
+});
+
+Route::controller(MessagesController::class)->group(function () {
+    Route::get('messages', 'index');
+    Route::get('messages/{message}', 'show');
+    Route::post('messages', 'store');
+    Route::patch('messages/{message}', 'update');
+    Route::delete('messages/{message}', 'destroy');
+});
+
+Route::apiResource("agencies", AgencyController::class);
+Route::apiResource("calendars", CalendarController::class);
+Route::apiResource("calendardates", CalendarDatesController::class);
+Route::apiResource("frequencies", FrequenciesController::class);
+Route::apiResource("routes", RoutesController::class);
+Route::apiResource("shapes", ShapesController::class);
+Route::apiResource("stops", StopsController::class);
+Route::apiResource("stoptimes", StopTimesController::class);
+Route::apiResource("trips", TripsController::class);
