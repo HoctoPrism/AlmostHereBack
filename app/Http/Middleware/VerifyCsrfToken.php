8ee357f5c1b0d5564be611beaad3d672ece92915
HoctoPrism <nemexxix@gmail.com>
2022-11-19 00:30:02 +0100

    changements de la config pour le csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -11,5 +11,7 @@ class VerifyCsrfToken extends Middleware
      *
      * @var array<int, string>
      */
-    protected $except = [];
+    protected $except = [
+        'http://127.0.0.1:8000/api/login'
+    ];
 }
