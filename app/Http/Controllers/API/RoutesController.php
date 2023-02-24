6fb4e143faacfd4f4376dd94345e3f3b43e3a2b4
HoctoPrism <nemexxix@gmail.com>
2023-02-24 18:18:24 +0100

    gestion par datetime de la route

--- app/Http/Controllers/API/RoutesController.php
+++ app/Http/Controllers/API/RoutesController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Routes;
-use App\Models\Trips;
 use Carbon\Carbon;
+use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 
 class RoutesController extends Controller
@@ -45,27 +45,60 @@ class RoutesController extends Controller
      */
     public function getOneRoute(Routes $route): JsonResponse
     {
-        $timeNow = Carbon::now()->format('H:m:s');
-        $timePlus1Hour = Carbon::now()->addHours(1)->format('H:m:s');
-        $actualDay = Carbon::now()->englishDayOfWeek;
+        return $this->getRoute($route);
+    }
+
+    /**
+     * Get all information for one route from a specific time
+     *
+     * @param Request $request
+     * @param Routes $route
+     * @return JsonResponse
+     */
+    public function scheduleRoute(Request $request, Routes $route): JsonResponse
+    {
+        $request->validate([
+            'time' => 'date|nullable',
+        ]);
+
+        return $this->getRoute($route, $request->time);
+    }
+
+    /**
+     * @param Routes $route
+     * @param $choosedTime
+     * @return JsonResponse
+     */
+    public function getRoute(Routes $route, $choosedTime = null): JsonResponse
+    {
+        if (isset($choosedTime)) {
+            $timeNow = Carbon::parse($choosedTime)->format('H:m:s');
+            $timePlus1Hour = Carbon::parse($choosedTime)->addHours(1)->format('H:m:s');
+            $day = strtolower(Carbon::parse($choosedTime)->englishDayOfWeek);
+            $date = Carbon::parse($choosedTime)->format('d/m');
+        } else {
+            $timeNow = Carbon::now()->format('H:m:s');
+            $timePlus1Hour = Carbon::now()->addHours(1)->format('H:m:s');
+            $day = Carbon::now()->englishDayOfWeek;
+            $date = 'auj';
+        }
 
         $route->load([
             'trips',
-            'trips.calendar'=> function ($query) use ($actualDay) {
-                $query->where($actualDay, '=', 1);
+            'trips.calendar' => function ($query) use ($day) {
+                $query->where($day, '=', 1);
             },
             /*'trips.calendar.calendarDates',*/
             'trips.stopTimes' => function ($query) use ($timeNow, $timePlus1Hour) {
                 $query
-                    ->whereBetween('departure_time', [$timeNow, $timePlus1Hour])
-                ;
+                    ->whereBetween('departure_time', [$timeNow, $timePlus1Hour]);
             },
             'trips.stopTimes.stop',
         ]);
 
         $trips = [];
-        foreach ($route->trips as $key => $trip){
-            if (!$trip->stopTimes->isEmpty()){
+        foreach ($route->trips as $key => $trip) {
+            if (!$trip->stopTimes->isEmpty()) {
                 $trips[] = $trip;
             }
         }
@@ -73,8 +106,9 @@ class RoutesController extends Controller
         $route['trips'] = $trips;
 
         return response()->json([
+            'time' => $choosedTime,
             'data' => $route,
-            'date' => $actualDay,
+            'date' => $date,
             'startTime' => Carbon::createFromFormat('H:m:s', $timeNow)->format('H'),
             'endTime' => Carbon::createFromFormat('H:m:s', $timePlus1Hour)->format('H'),
         ]);
