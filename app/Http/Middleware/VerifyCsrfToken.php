585db9c0248c18bc603a09013026272173f154ed
HoctoPrism <nemexxix@gmail.com>
2023-04-22 01:32:42 +0200

    change for prod + cpanel conf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'http://127.0.0.1:8000/api/login',
+        '/api/login',
         'api/routes/info/*',
         'api/itinary*'
     ];
